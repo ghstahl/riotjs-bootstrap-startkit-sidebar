11fa179543d7380f4258b675b30912b4befca534
Herb Stahl <ghstahl@gmail.com>
2017-05-18 13:34:21 -0700

    simple refactoring

--- src/extensions/riot-route-extension.js
+++ src/extensions/riot-route-extension.js
@@ -4,19 +4,16 @@ class RiotRouteExtension{
     self.name = 'RiotRouteExtension';
     self.namespace = self.name+':';
     self.currentPath = '';
-    self._bind();
-  }
-  
-  _bind(){
-    var self = this;
-    riot.route.parser((path)=>{
+
+    self._defaultParser = (path) =>{
       self.currentPath = path;
       return path.split(/[/?#]/)
-    },null);
-
-    riot.route.currentPath = ()=>{
+    }
+    self._getCurrentPath = () => {
       return self.currentPath;
-    };
+    }
+    riot.route.parser(self._defaultParser,null);
+    riot.route.currentPath = self._getCurrentPath;
   }
 }
 export default RiotRouteExtension;
